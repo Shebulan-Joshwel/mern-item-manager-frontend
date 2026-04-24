01c6a17174ed8437ba55e0aeef1c89629b20c6f0
Shebulan-Joshwel <Shebulan@outlook.com>
2026-04-24 15:42:19 +0530

    fix api url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "https://mern-item-manager-backend-ldzk.onrender.com";
 
 export const getItems = async () => {
   const res = await axios.get(`${API_URL}/items`);
@@ -16,4 +16,3 @@ export const deleteItem = async (id) => {
   const res = await axios.delete(`${API_URL}/items/${id}`);
   return res.data;
 };
-    
